c24d0264a24c1b06753e1d46523c4aac6f3d8ee2
NyshMysh1337 <killer.bir007@gmail.com>
2024-10-08 17:05:31 +0300

    Урок 4
    Выполнила практику 4-го урока:
    вместе с видео создала первую модель и
    первую миграцию
    выполнила д/з

---
+++ first_project/database/migrations/2024_10_08_135210_create_books_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('image')->nullable();
+            $table->text('description')->nullable();
+            $table->text('comment')->nullable();
+            $table->tinyInteger('mark')->nullable();
+            $table->boolean('is_audiobook')->default(false);
+            $table->boolean('is_printed_book')->default(false);
+            $table->boolean('is_ebook')->default(true);
+            $table->boolean('is_litres_book')->default(true);
+            $table->boolean('is_bought_audiobook')->default(false);
+            $table->boolean('is_bought_printed_book')->default(false);
+            $table->boolean('is_bought_ebook')->default(false);
+            $table->foreignId('cycles_id')->constrained('cycles')->nullable();
+            $table->foreignId('series_id')->constrained('series')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+};
