f30d183e8a2094fc60e1990b0d72e59dbc85a44c
NyshMysh1337 <killer.bir007@gmail.com>
2024-10-08 11:45:28 +0300

    Урок 3
    Добавила видимость для контроллеров
    Выполнила практику 3-го урока:
    вместе с видео создала первый контроллер,
    выполнила д/з

--- first_project/routes/web.php
+++ first_project/routes/web.php
@@ -1,5 +1,14 @@
 <?php
 
+use App\Http\Controllers\RoleAdminPageController;
+use App\Http\Controllers\RoleExpertPageController;
+use App\Http\Controllers\RolePageController;
+use App\Http\Controllers\RoleStudentPageController;
+use App\Http\Controllers\WelcomePageController;
+use App\Http\Controllers\MyPageAgeController;
+use App\Http\Controllers\MyPageController;
+use App\Http\Controllers\MyPageNameController;
+use App\Http\Controllers\MyPageYoutubeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,52 +23,20 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return 'Hello, world!';
+    return view('welcome');
 });
 
-Route::get('/my_page', function () {
-    return 'This is my page';
-});
-//д/з: создать еще 8 маршрутизаторов
-Route::get('/my_page/age', function () {
-    $year_birth = 2002;
-    $age = date('Y') - $year_birth;
-    return "I was born in {$year_birth}. Now I am {$age} years ago";
-});
-Route::get('/my_page/name', function () {
-    return 'Привет, моё имя Диана';
-});
-Route::get('/my_page/youtube', function () {
-    return '<iframe width="560" height="315" src="https://www.youtube.com/embed/WFciEoaPd64?si=TYUdhGd0vG8h2QHC"'
-        . ' title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media;'
-        . 'gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>';
-});
+Route::get('/my_page', [MyPageController::class, 'index']);
+//д/з(2): создать еще 8 маршрутизаторов
+//д/з(3): сщздать еще 8 контроллеров
+Route::get('/my_page/age', [MyPageAgeController::class, 'index']);
+Route::get('/my_page/name', [MyPageNameController::class, 'index']);
+Route::get('/my_page/youtube', [MyPageYoutubeController::class, 'index']);
+
 Route::redirect('/welcome', '/welcome/newcomer');
-Route::get('/welcome/{name}', function ($name) {
-    if ($name == 'newcomer') {
-        return "Добро пожаловать, незнакомец! <br/> Представься - введи в адрес свое имя";
-    }
-    return "Привет, {$name}";
-});
-Route::get('/role', function () {
-    $admin = '/role/admin';
-    $expert = '/role/expert';
-    $student = '/role/student';
-    return "Выбери свою роль: <br/>"
-        . "<p><a href={$admin}>Администратор</a></p>"
-        . "<p><a href={$expert}>Эксперт</a></p>"
-        . "<p><a href={$student}>Студент</a></p>";
-});
-Route::get('/role/admin', function () {
-    return 'Hi!';
-});
-Route::get('/role/expert', function () {
-    $url = 'https://expert-edu.ru/';
-    return "Добрый день! Со всей необходимой информацией Вы можете ознакомиться на сайте Гильдии экспертов"
-        . "<br/>ссылка: <a href={$url}>{$url}</a>";
-});
-Route::get('/role/student', function () {
-    $url = 'https://best-edu.ru/ratings/global/globalnyj-agregirovannyj-rejting';
-    return "Привет! Ознакомься с глобальным рейтингом вузов:"
-        . "<br/>ссылка: <a href={$url}>{$url}</a>";
-});
+Route::get('/welcome/{name}', [WelcomePageController::class, 'index']);
+
+Route::get('/role', [RolePageController::class, 'index']);
+Route::get('/role/admin', [RoleAdminPageController::class, 'index']);
+Route::get('/role/expert', [RoleExpertPageController::class, 'index']);
+Route::get('/role/student', [RoleStudentPageController::class, 'index']);
