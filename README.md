a1ebaf2a3b5c91d856313ef9b6c597dcc28cbf61
NyshMysh1337 <killer.bir007@gmail.com>
2024-10-08 10:53:03 +0300

    Сменила Laravel 10 на 9

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
 # Laravel_course
 Репозиторий, в котором храниться информация проделанная по курсу https://www.youtube.com/watch?v=BUiZDl3cokQ&amp;list=PLd2_Os8Cj3t8pnG4ubQemoqnTwf0VFEtU&amp;index=1&amp;ab_channel=LaravelCreative
 
-## Для установки Laravel через Docker
-https://www.youtube.com/watch?v=c6FbzmjC6oA 5.29(на работе)
+### Для установки Laravel через Docker
+https://www.youtube.com/watch?v=c6FbzmjC6oA
+### Чтобы установить 9 версию, а не последнюю
+Войти внутрь докер контейнера, внутрь bash и выполнить:  
+composer create-project laravel/laravel:^9.0 {имя проекта}
 
 ## Урок 0
 Установка Laravel через Composer (заменила на предыдущий пункт)
@@ -13,3 +16,4 @@ https://www.youtube.com/watch?v=c6FbzmjC6oA 5.29(на работе)
 ## Урок 2
 Первый route в laravel(маршрут). Меняем базовый вывод, добавляем еще один route.  
 Д/з: создать еще 8 routes.
+
--- first_project/routes/web.php
+++ first_project/routes/web.php
@@ -8,8 +8,8 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
 |
 */
 
@@ -20,60 +20,46 @@ Route::get('/', function () {
 Route::get('/my_page', function () {
     return 'This is my page';
 });
-
 //д/з: создать еще 8 маршрутизаторов
 Route::get('/my_page/age', function () {
     $year_birth = 2002;
     $age = date('Y') - $year_birth;
     return "I was born in {$year_birth}. Now I am {$age} years ago";
 });
-
 Route::get('/my_page/name', function () {
     return 'Привет, моё имя Диана';
 });
-
 Route::get('/my_page/youtube', function () {
     return '<iframe width="560" height="315" src="https://www.youtube.com/embed/WFciEoaPd64?si=TYUdhGd0vG8h2QHC"'
         . ' title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media;'
         . 'gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>';
 });
-
 Route::redirect('/welcome', '/welcome/newcomer');
-
 Route::get('/welcome/{name}', function ($name) {
     if ($name == 'newcomer') {
         return "Добро пожаловать, незнакомец! <br/> Представься - введи в адрес свое имя";
     }
     return "Привет, {$name}";
 });
-
 Route::get('/role', function () {
     $admin = '/role/admin';
     $expert = '/role/expert';
     $student = '/role/student';
-
     return "Выбери свою роль: <br/>"
         . "<p><a href={$admin}>Администратор</a></p>"
         . "<p><a href={$expert}>Эксперт</a></p>"
         . "<p><a href={$student}>Студент</a></p>";
 });
-
 Route::get('/role/admin', function () {
     return 'Hi!';
 });
-
 Route::get('/role/expert', function () {
     $url = 'https://expert-edu.ru/';
     return "Добрый день! Со всей необходимой информацией Вы можете ознакомиться на сайте Гильдии экспертов"
         . "<br/>ссылка: <a href={$url}>{$url}</a>";
 });
-
 Route::get('/role/student', function () {
     $url = 'https://best-edu.ru/ratings/global/globalnyj-agregirovannyj-rejting';
     return "Привет! Ознакомься с глобальным рейтингом вузов:"
         . "<br/>ссылка: <a href={$url}>{$url}</a>";
 });
-
-
-
-
