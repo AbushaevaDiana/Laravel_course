2974effe03337c912ab866b350ca5acaeedcd918
NyshMysh1337 <killer.bir007@gmail.com>
2024-10-09 17:30:36 +0300

    Урок 8
    Вытянула данные из бд с условиями по видео
    Выполнила д/з

--- first_project/app/Http/Controllers/PostController.php
+++ first_project/app/Http/Controllers/PostController.php
@@ -7,13 +7,40 @@ use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    public function index()
+    public function index(): void
     {
-        $post = Post::find(1);
+        $this->imageAll();
+        $this->imageContentOfAllUnpublished();
+        $this->imageTitleOfFirstWithoutImage();
+        $this->imageLikesOfAllPublishedWithoutImage();
+        dd('end');
+    }
+
+    private function imageAll(): void
+    {
+        $posts = Post::all();
+        dump($posts);
+    }
+
+    private function imageContentOfAllUnpublished(): void
+    {
+        $posts = Post::where('is_published', false)->get();
+        foreach ($posts as $post) {
+            dump($post->content);
+        }
+    }
+
+    private function imageLikesOfAllPublishedWithoutImage(): void
+    {
+        $posts = Post::where('image', null)->where('is_published', true)->get();
+        foreach ($posts as $post) {
+            dump($post->likes);
+        }
+    }
+
+    private function imageTitleOfFirstWithoutImage(): void
+    {
+        $post = Post::where('image', null)->first();
         dump($post->title);
-        dump($post->content);
-        dump($post->image);
-        dump($post->likes);
-        dd($post->is_published);
     }
 }
